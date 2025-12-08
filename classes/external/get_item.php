PelorusLabs/moodle-mod_workplacetraining

Initial commit for public release

classes/external/get_item.php
@@ -1,0 +1,105 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_workplacetraining\external;
+
+use context_module;
+use dml_exception;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_single_structure;
+use core_external\external_value;
+use mod_workplacetraining\local\section;
+use mod_workplacetraining\local\section_item;
+
+/**
+ * External function to get item data.
+ *
+ * @package    mod_workplacetraining
+ * @copyright  Pelorus Labs
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class get_item extends external_api {
+    /**
+     * Returns description of method parameters
+     *
+     * @return external_function_parameters
+     */
+    public static function execute_parameters() {
+        return new external_function_parameters(
+            [
+                'id' => new external_value(PARAM_INT, 'Item ID'),
+            ]
+        );
+    }
+
+    /**
+     * Get section item data
+     *
+     * @param int $id Section item ID
+     * @return array
+     * @throws dml_exception
+     */
+    public static function execute(int $id) {
+        global $DB;
+
+        $params = self::validate_parameters(
+            self::execute_parameters(),
+            ['id' => $id]
+        );
+
+        $item = new section_item($params['id']);
+        $section = new section($item->get('sectionid'));
+
+        $workplacetraining = $DB->get_record('workplacetraining', ['id' => $section->get('wtid')], '*', MUST_EXIST);
+        $cm = get_coursemodule_from_instance('workplacetraining', $workplacetraining->id, $workplacetraining->course);
+
+        $context = context_module::instance($cm->id);
+        self::validate_context($context);
+        require_capability('mod/workplacetraining:manage', $context);
+
+        return [
+            'id' => $item->get('id'),
+            'sectionid' => $item->get('sectionid'),
+            'name' => $item->get('name'),
+            'description' => $item->get('description'),
+            'isrequired' => $item->get('isrequired'),
+            'type' => $item->get('type'),
+            'position' => $item->get('position'),
+            'config' => json_encode($item->get_type_instance()->get_config($item)),
+        ];
+    }
+
+    /**
+     * Returns description of method result value
+     *
+     * @return external_single_structure
+     */
+    public static function execute_returns() {
+        return new external_single_structure(
+            [
+                'id' => new external_value(PARAM_INT, 'ID of the section'),
+                'sectionid' => new external_value(PARAM_INT, 'ID of the section'),
+                'name' => new external_value(PARAM_TEXT, 'Section name'),
+                'description' => new external_value(PARAM_TEXT, 'Section description'),
+                'isrequired' => new external_value(PARAM_BOOL, 'Is item required for completion'),
+                'type' => new external_value(PARAM_TEXT, 'Item type'),
+                'position' => new external_value(PARAM_INT, 'Position of the section'),
+                'config' => new external_value(PARAM_TEXT, 'Item type config', VALUE_OPTIONAL),
+            ]
+        );
+    }
+}

classes/external/new_evaluation.php
@@ -1,0 +1,104 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_workplacetraining\external;
+
+use context_module;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_single_structure;
+use core_external\external_value;
+use mod_workplacetraining\local\evaluation;
+
+/**
+ * External function to create a new evaluation.
+ *
+ * @package    mod_workplacetraining
+ * @copyright  Pelorus Labs
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class new_evaluation extends external_api {
+    /**
+     * Returns description of method parameters
+     *
+     * @return external_function_parameters
+     */
+    public static function execute_parameters(): external_function_parameters {
+        return new external_function_parameters(
+            [
+                'wtid' => new external_value(PARAM_INT, 'Workplace training id'),
+                'userid' => new external_value(PARAM_INT, 'User id'),
+            ]
+        );
+    }
+
+    /**
+     * Create a new evaluation
+     *
+     * @param int $wtid
+     * @param int $userid
+     * @return array
+     */
+    public static function execute(int $wtid, int $userid) {
+        global $DB;
+
+        $params = self::validate_parameters(
+            self::execute_parameters(),
+            ['wtid' => $wtid, 'userid' => $userid]
+        );
+
+        $workplacetraining = $DB->get_record('workplacetraining', ['id' => $wtid], '*', MUST_EXIST);
+        $cm = get_coursemodule_from_instance('workplacetraining', $workplacetraining->id, $workplacetraining->course);
+
+        $context = context_module::instance($cm->id);
+        self::validate_context($context);
+        require_capability('mod/workplacetraining:newevaluation', $context);
+
+        if (!$DB->record_exists('user', ['id' => $params['userid']])) {
+            return false;
+        }
+
+        $evaluation = evaluation::get_active_evaluation($workplacetraining->id, $userid);
+        if ($evaluation === false) {
+            $evaluation = evaluation::get_record_create_if_not_exists($workplacetraining->id, $userid);
+        }
+
+        if (!$evaluation->is_finalised()) {
+            return false;
+        }
+
+        $newevaluation = $evaluation->create_new_version();
+
+        return [
+            'success' => true,
+            'version' => $newevaluation->get('version'),
+        ];
+    }
+
+    /**
+     * Returns description of method result value
+     *
+     * @return external_single_structure
+     */
+    public static function execute_returns(): external_single_structure {
+        return new external_single_structure(
+            [
+                'success' => new external_value(PARAM_BOOL, 'Status of the operation'),
+                'version' => new external_value(PARAM_INT, 'Version number of the new evaluation'),
+            ]
+        );
+    }
+}

classes/local/response.php
@@ -1,0 +1,65 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_workplacetraining\local;
+
+/**
+ * Class for response data
+ *
+ * @package    mod_workplacetraining
+ * @copyright  Pelorus Labs
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class response extends \core\persistent {
+    /**
+     * Database data.
+     */
+    public const TABLE = 'workplacetraining_responses';
+
+    /**
+     * Defines and returns the properties of the class.
+     *
+     * @return array
+     */
+    protected static function define_properties() {
+        return [
+            'itemid' => [
+                'type' => PARAM_INT,
+                'description' => 'The item ID.',
+            ],
+            'userid' => [
+                'type' => PARAM_INT,
+                'description' => 'The user ID.',
+            ],
+            'response' => [
+                'type' => PARAM_TEXT,
+                'description' => 'Response data.',
+                'null' => NULL_ALLOWED,
+                'default' => null,
+            ],
+            'completed' => [
+                'type' => PARAM_BOOL,
+                'description' => 'Whether the item is completed',
+                'default' => false,
+            ],
+            'version' => [
+                'type' => PARAM_INT,
+                'description' => 'The version of the response.',
+                'default' => 1,
+            ],
+        ];
+    }
+}

cli/reorder_positions.php
@@ -1,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Reorder all of the positions of all sections and subsections.
+ *
+ * @package   mod_workplacetraining
+ * @copyright Pelorus Labs
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('CLI_SCRIPT', true);
+
+require(__DIR__ . '/../../../config.php');
+
+// Subsections.
+$sections = \mod_workplacetraining\local\section::get_records();
+foreach ($sections as $section) {
+    $section->reorder_subsection_positions();
+}
+
+// Top levels.
+$position = 0;
+foreach (\mod_workplacetraining\local\section::get_records(['parentsection' => null], 'position') as $subsection) {
+    $subsection->set('position', $position);
+    $subsection->update();
+    $position++;
+
+    $itempos = 0;
+    foreach ($subsection->get_items() as $item) {
+        $item->set('position', $itempos);
+        $item->update();
+        $itempos++;
+    }
+}

tests/generator/lib.php
@@ -1,0 +1,50 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Workplace training module data generator class
+ *
+ * @package    mod_workplacetraining
+ * @category   test
+ * @copyright  Pelorus Labs
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_workplacetraining_generator extends testing_module_generator {
+    /**
+     * Create a new instance of the workplacetraining activity.
+     *
+     * @param array|stdClass|null $record
+     * @param array|null $options
+     * @return stdClass
+     */
+    public function create_instance($record = null, ?array $options = null) {
+        $record = (object)(array)$record;
+
+        $defaultsettings = [
+            'name' => 'Test Workplace Training',
+            'intro' => 'Test workplace training description',
+            'introformat' => FORMAT_HTML,
+        ];
+
+        foreach ($defaultsettings as $name => $value) {
+            if (!isset($record->{$name})) {
+                $record->{$name} = $value;
+            }
+        }
+
+        return parent::create_instance($record, (array)$options);
+    }
+}

PelorusLabs/moodle-mod_trainingevaluation

classes/external/get_item.php
@@ -1,0 +1,105 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_trainingevaluation\external;
+
+use context_module;
+use dml_exception;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_single_structure;
+use core_external\external_value;
+use mod_trainingevaluation\local\section;
+use mod_trainingevaluation\local\section_item;
+
+/**
+ * External function to get item data.
+ *
+ * @package    mod_trainingevaluation
+ * @copyright  Pelorus Labs
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class get_item extends external_api {
+    /**
+     * Returns description of method parameters
+     *
+     * @return external_function_parameters
+     */
+    public static function execute_parameters() {
+        return new external_function_parameters(
+            [
+                'id' => new external_value(PARAM_INT, 'Item ID'),
+            ]
+        );
+    }
+
+    /**
+     * Get section item data
+     *
+     * @param int $id Section item ID
+     * @return array
+     * @throws dml_exception
+     */
+    public static function execute(int $id) {
+        global $DB;
+
+        $params = self::validate_parameters(
+            self::execute_parameters(),
+            ['id' => $id]
+        );
+
+        $item = new section_item($params['id']);
+        $section = new section($item->get('sectionid'));
+
+        $trainingevaluation = $DB->get_record('trainingevaluation', ['id' => $section->get('wtid')], '*', MUST_EXIST);
+        $cm = get_coursemodule_from_instance('trainingevaluation', $trainingevaluation->id, $trainingevaluation->course);
+
+        $context = context_module::instance($cm->id);
+        self::validate_context($context);
+        require_capability('mod/trainingevaluation:manage', $context);
+
+        return [
+            'id' => $item->get('id'),
+            'sectionid' => $item->get('sectionid'),
+            'name' => $item->get('name'),
+            'description' => $item->get('description'),
+            'isrequired' => $item->get('isrequired'),
+            'type' => $item->get('type'),
+            'position' => $item->get('position'),
+            'config' => json_encode($item->get_type_instance()->get_config($item)),
+        ];
+    }
+
+    /**
+     * Returns description of method result value
+     *
+     * @return external_single_structure
+     */
+    public static function execute_returns() {
+        return new external_single_structure(
+            [
+                'id' => new external_value(PARAM_INT, 'ID of the section'),
+                'sectionid' => new external_value(PARAM_INT, 'ID of the section'),
+                'name' => new external_value(PARAM_TEXT, 'Section name'),
+                'description' => new external_value(PARAM_TEXT, 'Section description'),
+                'isrequired' => new external_value(PARAM_BOOL, 'Is item required for completion'),
+                'type' => new external_value(PARAM_TEXT, 'Item type'),
+                'position' => new external_value(PARAM_INT, 'Position of the section'),
+                'config' => new external_value(PARAM_TEXT, 'Item type config', VALUE_OPTIONAL),
+            ]
+        );
+    }
+}

classes/external/new_evaluation.php
@@ -1,0 +1,104 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_trainingevaluation\external;
+
+use context_module;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_single_structure;
+use core_external\external_value;
+use mod_trainingevaluation\local\evaluation;
+
+/**
+ * External function to create a new evaluation.
+ *
+ * @package    mod_trainingevaluation
+ * @copyright  Pelorus Labs
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class new_evaluation extends external_api {
+    /**
+     * Returns description of method parameters
+     *
+     * @return external_function_parameters
+     */
+    public static function execute_parameters(): external_function_parameters {
+        return new external_function_parameters(
+            [
+                'wtid' => new external_value(PARAM_INT, 'Training evaluation id'),
+                'userid' => new external_value(PARAM_INT, 'User id'),
+            ]
+        );
+    }
+
+    /**
+     * Create a new evaluation
+     *
+     * @param int $wtid
+     * @param int $userid
+     * @return array
+     */
+    public static function execute(int $wtid, int $userid) {
+        global $DB;
+
+        $params = self::validate_parameters(
+            self::execute_parameters(),
+            ['wtid' => $wtid, 'userid' => $userid]
+        );
+
+        $trainingevaluation = $DB->get_record('trainingevaluation', ['id' => $wtid], '*', MUST_EXIST);
+        $cm = get_coursemodule_from_instance('trainingevaluation', $trainingevaluation->id, $trainingevaluation->course);
+
+        $context = context_module::instance($cm->id);
+        self::validate_context($context);
+        require_capability('mod/trainingevaluation:newevaluation', $context);
+
+        if (!$DB->record_exists('user', ['id' => $params['userid']])) {
+            return false;
+        }
+
+        $evaluation = evaluation::get_active_evaluation($trainingevaluation->id, $userid);
+        if ($evaluation === false) {
+            $evaluation = evaluation::get_record_create_if_not_exists($trainingevaluation->id, $userid);
+        }
+
+        if (!$evaluation->is_finalised()) {
+            return false;
+        }
+
+        $newevaluation = $evaluation->create_new_version();
+
+        return [
+            'success' => true,
+            'version' => $newevaluation->get('version'),
+        ];
+    }
+
+    /**
+     * Returns description of method result value
+     *
+     * @return external_single_structure
+     */
+    public static function execute_returns(): external_single_structure {
+        return new external_single_structure(
+            [
+                'success' => new external_value(PARAM_BOOL, 'Status of the operation'),
+                'version' => new external_value(PARAM_INT, 'Version number of the new evaluation'),
+            ]
+        );
+    }
+}

classes/local/response.php
@@ -1,0 +1,65 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_trainingevaluation\local;
+
+/**
+ * Class for response data
+ *
+ * @package    mod_trainingevaluation
+ * @copyright  Pelorus Labs
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class response extends \core\persistent {
+    /**
+     * Database data.
+     */
+    public const TABLE = 'trainingevaluation_responses';
+
+    /**
+     * Defines and returns the properties of the class.
+     *
+     * @return array
+     */
+    protected static function define_properties() {
+        return [
+            'itemid' => [
+                'type' => PARAM_INT,
+                'description' => 'The item ID.',
+            ],
+            'userid' => [
+                'type' => PARAM_INT,
+                'description' => 'The user ID.',
+            ],
+            'response' => [
+                'type' => PARAM_TEXT,
+                'description' => 'Response data.',
+                'null' => NULL_ALLOWED,
+                'default' => null,
+            ],
+            'completed' => [
+                'type' => PARAM_BOOL,
+                'description' => 'Whether the item is completed',
+                'default' => false,
+            ],
+            'version' => [
+                'type' => PARAM_INT,
+                'description' => 'The version of the response.',
+                'default' => 1,
+            ],
+        ];
+    }
+}

cli/reorder_positions.php
@@ -1,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Reorder all of the positions of all sections and subsections.
+ *
+ * @package   mod_trainingevaluation
+ * @copyright Pelorus Labs
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('CLI_SCRIPT', true);
+
+require(__DIR__ . '/../../../config.php');
+
+// Subsections.
+$sections = \mod_trainingevaluation\local\section::get_records();
+foreach ($sections as $section) {
+    $section->reorder_subsection_positions();
+}
+
+// Top levels.
+$position = 0;
+foreach (\mod_trainingevaluation\local\section::get_records(['parentsection' => null], 'position') as $subsection) {
+    $subsection->set('position', $position);
+    $subsection->update();
+    $position++;
+
+    $itempos = 0;
+    foreach ($subsection->get_items() as $item) {
+        $item->set('position', $itempos);
+        $item->update();
+        $itempos++;
+    }
+}

tests/generator/lib.php
@@ -1,0 +1,50 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Training evaluation module data generator class
+ *
+ * @package    mod_trainingevaluation
+ * @category   test
+ * @copyright  Pelorus Labs
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_trainingevaluation_generator extends testing_module_generator {
+    /**
+     * Create a new instance of the trainingevaluation activity.
+     *
+     * @param array|stdClass|null $record
+     * @param array|null $options
+     * @return stdClass
+     */
+    public function create_instance($record = null, ?array $options = null) {
+        $record = (object)(array)$record;
+
+        $defaultsettings = [
+            'name' => 'Test Training Evaluation',
+            'intro' => 'Test training evaluation description',
+            'introformat' => FORMAT_HTML,
+        ];
+
+        foreach ($defaultsettings as $name => $value) {
+            if (!isset($record->{$name})) {
+                $record->{$name} = $value;
+            }
+        }
+
+        return parent::create_instance($record, (array)$options);
+    }
+}